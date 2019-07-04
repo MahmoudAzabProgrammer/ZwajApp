7a27c5054f9f367385dcb9a6a84e854d77f46110
Mahmoud Azab <mahmoud.azab1001@gmail.com>
2019-07-04 20:47:14 +0200

    End Of Section 4

--- ZwajApp-SPA/src/app/value/value.component.ts
+++ ZwajApp-SPA/src/app/value/value.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -9,18 +9,20 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./value.component.css']
 })
 export class ValueComponent implements OnInit {
-
   values: any;
+  @Input() valuesFromRegister: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
+   // this.getValues();
   }
+  /*
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(
       response => { this.values = response; },
       error => { console.log(error); }
     )
   }
-
+  */
+  
 }
--- ZwajApp.API/Controllers/ValuesController.cs
+++ ZwajApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace ZwajApp.API.Controllers
         }
         // GET http://localhost:5000/api/values
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Get()
         {
